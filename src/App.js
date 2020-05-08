0e06ba40889eaca4400f54f66ab0a5c2bae2afd3
Raul Martins <ramalima@brasilseg.com.br>
2020-05-08 18:23:31 -0300

    cria button ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Title from "./components/title";
+import Button from "./components/button";
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Button />
     </div>
   );
 }
---
+++ src/components/button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function button() {
+  return <button type="button">ok</button>;
+}
+
+export default button;
