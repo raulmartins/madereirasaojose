059b3dbdf6fd759dba527a470fa0d33c1a3282f1
Raul Martins <ramalima@brasilseg.com.br>
2020-05-08 18:52:07 -0300

    es6

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,7 +3,7 @@ import React from "react";
 // import { Container } from './styles';
 
 function button() {
-  return <button type="button">estaca</button>;
+  return <button type="button">es6</button>;
 }
 
 export default button;
